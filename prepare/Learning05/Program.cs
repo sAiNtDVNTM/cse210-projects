3419da3b76a303f3fa82dcb4d43ff1c7396b5c6d
sAiNtDVNTM <josedrago2011@hotmail.com>
2023-11-28 00:15:13 -0600

    Week 5 done

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> sList = new List<Shape>();
+
+        Square s1 = new Square("SkyBlue", 4);
+        sList.Add(s1);
+
+        Rectangle s2 = new Rectangle("Purple", 8, 5);
+        sList.Add(s2);
+
+        Circle s3 = new Circle("Yellow", 3);
+        sList.Add(s3);
+
+        foreach(Shape s in sList)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
+
     }
 }
